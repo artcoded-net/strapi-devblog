886f63d42ec87fda23b4b2766f44b8da8a7fbea3
Artcoded <dev@artcoded.net>
2022-05-27 17:20:07 +0200

    add cloudinary for upload

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -14,10 +14,24 @@ module.exports = ({ env }) => ({
         // ... any custom nodemailer options
       },
       settings: {
-        defaultFrom: "info@artocoded.net",
+        defaultFrom: "info@artcoded.net",
         defaultReplyTo: "info@artcoded.net",
       },
     },
   },
-  // ...
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
 });
