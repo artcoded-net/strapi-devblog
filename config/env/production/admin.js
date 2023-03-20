9a76cc09737589e8fd0199c365856e6793e18cdf
Artcoded <dev@artcoded.net>
2023-03-20 12:20:21 +0100

    next course

--- config/env/production/admin.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => {
-  // know if we want to serve the admin panel
-  // if not, we want to know the absolute URL of the admin panel (admin.artcoded.net)
-
-  return {
-    url: env("PUBLIC_ADMIN_URL", "/dashboard"),
-    serveAdminPanel: env("PUBLIC_ADMIN_URL") == undefined,
-  };
-};
